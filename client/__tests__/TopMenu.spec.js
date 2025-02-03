babefe1a017128c060c37ccad9f212eb6a9065b8
Christopher Ray <89406359+cray4427@users.noreply.github.com>
2025-02-03 19:16:24 +0000

    fixed topmenu test

--- client/__tests__/TopMenu.spec.js
+++ client/__tests__/TopMenu.spec.js
@@ -1,9 +1,37 @@
-import { mount } from '@vue/test-utils'
-import MenubarComponent from '@/components/topmenu/TopMenu.vue' // Adjust to your file path
-import { createPinia } from 'pinia'
-import { useTokenStore } from '@/stores/Token'
-import { nextTick } from 'vue'
+import { flushPromises, mount } from '@vue/test-utils';
+import { describe, it, vi } from 'vitest';
+import TopMenu from '@/components/topmenu/TopMenu.vue';
+import { useTokenStore } from '@/stores/Token';
+import { createTestingPinia } from '@pinia/testing';
 
-//need to look at the token store a little bit more to understand how to test this component
-describe.todo('TopMenu', () => {
-})
+
+describe('MyComponent', () => {
+  it('renders menu for admin user', async () => {
+
+    const wrapper = mount(TopMenu, {
+        global: {
+          plugins: [createTestingPinia(
+            {
+                stubActions: false,
+            },
+        )],
+        },
+    })
+
+    const tokenStore = useTokenStore()
+
+    //apply mock values from token store
+    tokenStore.is_admin = true
+    tokenStore.is_user = true
+
+    //patch the token so that $subscibe listener is triggered
+    tokenStore.$patch({ token: ''})
+
+    // await wrapper.vm.$nextTick();
+
+    console.log(wrapper.vm.items)
+
+    // Assert that the admin menu items are rendered
+    expect(wrapper.vm.items).toHaveLength(8);
+  });
+});
