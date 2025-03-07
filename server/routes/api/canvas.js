7457b1ae4fbd6a28be2856594adf10e8e24eef49
Devon-Hill7 <123010104+Devon-Hill7@users.noreply.github.com>
2025-03-07 22:48:45 +0000

    Added swagger comments to canvas and email API

--- server/routes/api/canvas.js
+++ server/routes/api/canvas.js
@@ -3,7 +3,40 @@ import canvasService from '../../services/canvasService.js';
 import logger from '../../configs/logger.js';
 
 const router = express.Router();
-
+/**
+ * @swagger
+ * /courses:
+ *   get:
+ *     summary: Retrieve a list of courses
+ *     description: Fetches the list of courses from the Canvas API.
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved courses
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *       401:
+ *         description: Unauthorized - Canvas API is not configured
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *       503:
+ *         description: Service unavailable - Failed to get courses
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ */
 router.get('/courses', async function (req, res){
     const response = await canvasService.getCourses();
     
