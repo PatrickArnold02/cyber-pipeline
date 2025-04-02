6632a5b20b27e1b9b3731077dbf3bfd487c70ea8
Christopher Ray <89406359+cray4427@users.noreply.github.com>
2025-04-02 17:43:52 +0000

    changes

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -68,57 +68,77 @@
       </div>
 
       <div v-if="selectedContent === 'curriculum'">
-        <div class="p-8">
-
         <!-- Course List Section -->
         <section>
-          <h2 class="text-2xl font-semibold text-gray-800 mb-4">Course List</h2>
-          <p class="text-lg text-gray-700 mb-4">The Cyber Pipeline Curriculum contains several courses from K-State's Computational Core that have been aligned with AP curriculum standards. These are the same courses we use in our Computer Science Certificate and Integrated Computer Science degree programs, so students are getting the real college experience! Currently available courses include:</p>
+          <h1 id="top" class="p-title">Course List</h1>
+          <p class="text-lg mb-4">The Cyber Pipeline Curriculum contains several courses from K-State's Computational Core that have been aligned with AP curriculum standards. These are the same courses we use in our Computer Science Certificate and Integrated Computer Science degree programs, so students are getting the real college experience! Currently available courses include:</p>
 
           <ul class="list-disc pl-8 space-y-2">
-            <li class="text-lg text-gray-700">CC 110 – Introduction to Computing</li>
-            <li class="text-lg text-gray-700">CC 210 – Fundamental Computer Programming Concepts</li>
+            <li class="text-lg font-bold">CC 110 – Introduction to Computing</li>
+            <li class="text-lg font-bold">CC 210 – Fundamental Computer Programming Concepts</li>
           </ul>
 
-          <p class="text-lg text-gray-700 mt-4">Beyond the introductory courses, many advanced courses are also available for schools wanting to go further or bolster their existing curricula. These include:</p>
+          <p class="text-lg mt-4 mb-4">Beyond the introductory courses, many advanced courses are also available for schools wanting to go further or bolster their existing curricula. These include:</p>
 
-          <ul class="list-disc pl-8 space-y-2">
-            <li class="text-lg text-gray-700">CC 120 – Web Page Design</li>
-            <li class="text-lg text-gray-700">CC 310 – Data Structures & Algorithms I</li>
-            <li class="text-lg text-gray-700">CC 310 – Data Structures & Algorithms II</li>
-            <li class="text-lg text-gray-700">CC 410 – Advanced Programming</li>
+          <ul class="list-disc pl-8 space-y-2 mb-4">
+            <li class="text-lg font-bold">CC 120 – Web Page Design</li>
+            <li class="text-lg font-bold">CC 310 – Data Structures & Algorithms</li>
+            <li class="text-lg font-bold">CC 410 – Advanced Programming</li>
           </ul>
         </section>
 
         <!-- College Credit Options Section -->
-        <section class="mb-12">
-          <h2 class="text-2xl font-semibold text-gray-800 mb-4">College Credit Options</h2>
-          <p class="text-lg text-gray-700">Students taking these courses may choose to take the associated AP exam to gain college credit that is accepted nationwide. We can also work with your local community or technical college to explore options for college credit through the Kansas Career and Technical Education (CTE) program. Finally, any students who attend K-State in a CS degree or certificate program may also earn credit by examination for many of these courses, jumpstarting their college career.</p>
+        <section class="mb-4">
+          <h2 class="text-2xl font-semibold mb-2">College Credit Options</h2>
+          <p class="text-lg ">Students taking these courses may choose to take the associated AP exam to gain college credit that is accepted nationwide. We can also work with your local community or technical college to explore options for college credit through the Kansas Career and Technical Education (CTE) program. Finally, any students who attend K-State in a CS degree or certificate program may also earn credit by examination for many of these courses, jumpstarting their college career.</p>
         </section>
 
         <!-- Courses Section -->
         <section>
-          <h2 class="text-2xl font-semibold text-gray-800 mb-4">Courses</h2>
+          <h2 class="text-2xl font-semibold mb-4">Course Descriptions</h2>
 
           <div class="mb-8">
-            <h3 class="text-xl font-semibold text-gray-800 mb-2">CC 110 – Introduction to Computing</h3>
-            <p class="text-lg text-gray-700 mb-2">CC 110 is the first course in our sequence, primarily for students with little to no experience in programming or computer science. The target audience is students in high school with at least some experience in Algebra.</p>
-            <p class="text-lg text-gray-700 mb-2">The course serves as a broad introduction to the field of computer science, covering many of the big ideas and innovators throughout history, as well as the various subfields of computer science such as artificial intelligence, cybersecurity, and more.</p>
-            <p class="text-lg text-gray-700 mb-2">In addition, students will learn the basics of programming in both pseudocode and Python, preparing them for future courses that dive deeper into the core concepts of programming.</p>
-            <p class="text-lg text-gray-700">This course is targeted to align with the AP Computer Science Principles (CS P) curriculum. See the AP Syllabus for a detailed overview of how the CC 110 course aligns with AP CS P.</p>
+            <h3 class="text-xl font-semibold mb-2">CC 110 – Introduction to Computing</h3>
+            <p class="text-lg mb-2">CC 110 is the first course in our sequence, primarily for students with little to no experience in programming or computer science. The target audience is students in high school with at least some experience in Algebra.</p>
+            <p class="text-lg mb-2">The course serves as a broad introduction to the field of computer science, covering many of the big ideas and innovators throughout history, as well as the various subfields of computer science such as artificial intelligence, cybersecurity, and more.</p>
+            <p class="text-lg mb-2">In addition, students will learn the basics of programming in both pseudocode and Python, preparing them for future courses that dive deeper into the core concepts of programming.</p>
+            <p class="text-lg ">This course is targeted to align with the AP Computer Science Principles (CS P) curriculum. See the AP Syllabus for a detailed overview of how the CC 110 course aligns with AP CS P.</p>
           </div>
 
           <div>
-            <h3 class="text-xl font-semibold text-gray-800 mb-2">CC 210 – Fundamental Computer Programming Concepts</h3>
-            <p class="text-lg text-gray-700 mb-2">CC 210 is intended to be the second course in the sequence, but it can also be used with students who have prior programming experience.</p>
-            <p class="text-lg text-gray-700 mb-2">This course introduces programming concepts in-depth using Java, starting with basic data types and variables, conditional and loop statements, functions, strings, and aggregate data types (arrays and lists). Students then shift to object-oriented programming, learning about classes, objects, and inheritance to set the stage for future coursework in data structures and algorithms.</p>
-            <p class="text-lg text-gray-700 mb-2">This course is targeted to align with the AP Computer Science A (CS A) curriculum. See the AP Syllabus (link coming soon) for a detailed overview of how the CC 210 course aligns with AP CS A.</p>
+            <h3 class="text-xl font-semibold mb-2">CC 210 – Fundamental Computer Programming Concepts</h3>
+            <p class="text-lg mb-2">CC 210 is intended to be the second course in the sequence, but it can also be used with students who have prior programming experience.</p>
+            <p class="text-lg mb-2">This course introduces programming concepts in-depth using Java, starting with basic data types and variables, conditional and loop statements, functions, strings, and aggregate data types (arrays and lists). Students then shift to object-oriented programming, learning about classes, objects, and inheritance to set the stage for future coursework in data structures and algorithms.</p>
+            <p class="text-lg mb-2">This course is targeted to align with the AP Computer Science A (CS A) curriculum. See the AP Syllabus (link coming soon) for a detailed overview of how the CC 210 course aligns with AP CS A.</p>
           </div>
         </section>
 
-        </div>
       </div>
 
+      <div v-if="selectedContent === 'teacher-support'">
+        <!-- Course List Section -->
+        <section>
+          <h1 id="top" class="p-title">Teacher Support</h1>
+
+          <h2 class="text-2xl font-semibold mb-1">Training</h2>
+          <p class="text-lg mb-4">The Cyber Pipeline program includes an engaging and robust professional development program 
+            designed to prepare a teacher with no background in computer science to teach the 
+            <span class="font-bold">CC 110 and/or CC 210 courses in under a year.</span> 
+            The training is delivered asynchronously online, allowing you to fit it into your schedule. 
+            Additionally, there is a synchronous (but also available as a recording) online seminar series held the first Friday of each month. 
+            Finally, teachers engaging in the program have continuous access to mentoring by Computer Science and Pedagogy experts drawn from 
+            K-State faculty through instant messaging, email, and video collaboration.
+          </p>
+
+          <h2 class="text-2xl font-semibold mb-1">Professional Development</h2>
+          <p class="text-lg mb-4">This professional development is offered in a partnership between the K-State Computer Science Department 
+            and the College of Education, and awards 10 graduate credit hours that can also be applied to a Graduate Interdisciplinary
+            Certificate in Computer Science Education* and/or a Masters in Curriculum and Instruction with a focus on Computer Science Education.
+          </p>
+
+        </section>
+
+      </div>
     </main>
   </div>
 </template>
@@ -151,7 +171,9 @@ onMounted(() => {
     {
       label: 'Teacher Support',
       icon: 'pi pi-fw pi-video',
-      command: () => alert('Teacher Support page coming soon!')
+      command: () => {
+        selectedContent.value = 'teacher-support'; // Show the curriculum content
+      }
     },
     {
       label: 'How To Join',
